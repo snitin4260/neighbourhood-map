240b276307f305d94eed88effb8ed54bf9133ed1
nitin <snitin8994@gmail.com>
2018-11-26 10:03:14 +0530

    Updates

---
+++ precache-manifest.81b0b7f7dbfa873f94baf972018f355b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "676e9883ac50e32bf17c",
+    "url": "/static/css/main.ad41750d.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "97fca8c6e998a8a3598d",
+    "url": "/static/js/1.97fca8c6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "676e9883ac50e32bf17c",
+    "url": "/static/js/main.676e9883.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "97fca8c6e998a8a3598d",
+    "url": "/static/css/1.64b2568e.chunk.css"
+  },
+  {
+    "revision": "3e9ffef074af4327c184fa356783521d",
+    "url": "/index.html"
+  }
+];
